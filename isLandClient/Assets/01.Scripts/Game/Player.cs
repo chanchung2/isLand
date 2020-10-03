83c5be021251ad9fce53b5e98d19847fc930d0b0
qkrgksquf00 <qkrgksquf00@naver.com>
2020-10-04 04:48:05 +0900

    - Refactoring Player

--- isLandClient/Assets/01.Scripts/Game/Player.cs
+++ isLandClient/Assets/01.Scripts/Game/Player.cs
@@ -1,12 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.U2D;
 
 public class Player : MonoBehaviour
 {
     public enum kMOVE_DIRECTION
     {
-        None,
         Up,
         LeftUp,
         Left,
@@ -31,24 +32,57 @@ public class Player : MonoBehaviour
 
     private Vector3 m_BaseCharacterScale;
 
-#region Animation Parameter Name
-    private readonly string kCHARACTER_WALK_FRONT = "Walk_Front";
-    private readonly string kCHARACTER_WALK_SIDE = "Walk_Side";
+    private SpriteAtlas m_SpriteAtlasCharacter;
 
-    private readonly string kCHARACTER_IDLE_FRONT = "Idle_Front";
-    private readonly string kCHARACTER_IDLE_SIDE = "Idle_Side";
-    private readonly string kCHARACTER_IDLE_BACK = "Idle_Back";
+    [Header("SpriteSlot")]
+    [SerializeField]
+    private SpriteRenderer m_SpriteCharacter;
+    [SerializeField]
+    private SpriteRenderer m_SpriteTop;
+    [SerializeField]
+    private SpriteRenderer m_SpritePants;
+    [SerializeField]
+    private SpriteRenderer m_SpriteEyes;
+    [SerializeField]
+    private SpriteRenderer m_SpriteHair;
+
+    [Header("SpriteName")]
+    [SerializeField]
+    private string kSPRITE_NAME_CHARATER;
+    [SerializeField]
+    private string kSPRITE_NAME_TOP;
+    [SerializeField]
+    private string kSPRITE_NAME_PANTS;
+    [SerializeField]
+    private string kSPRITE_NAME_EYES;
+    [SerializeField]
+    private string kSPRITE_NAME_HAIR;
+
+    [Header("SpriteDirectionName")]
+    [SerializeField]
+    private string kSPRITE_NAME_DIRECTION_SIDE;
+    [SerializeField]
+    private string kSPRITE_NAME_DIRECTION_BACK;
+
+#region Animation Parameter Name
+    private readonly string kPARAMETER_NAME_WALK = "Walk";
 #endregion
 
     void Start()
     {
         m_BaseCharacterScale = this.transform.localScale;
+
+        m_SpriteAtlasCharacter = Resources.Load<SpriteAtlas>("Atlas/Character");
     }
 
     void Update()
     {
 #if UNITY_EDITOR
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            Move(kMOVE_DIRECTION.Up);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
         {
             Move(kMOVE_DIRECTION.Left);
         }
@@ -62,99 +96,77 @@ public class Player : MonoBehaviour
         }
         else
         {
-            Move(kMOVE_DIRECTION.None);
-        }
-
-        if (m_CurrentState == kCHARACTER_STATE.Idle)
-        {
-            CharacterIdleUpdate();   
+            StopMove();
         }
 #endif
     }
 
-    // 캐릭터의 방향 업데이트.
-    private void CharacterIdleUpdate()
+    private void UpdateSprite(int index)
     {
-        switch (m_CurrentDirection)
-        {
-            case kMOVE_DIRECTION.Up :
-                break;
-            case kMOVE_DIRECTION.Down :
-                m_Animator.SetBool(kCHARACTER_IDLE_FRONT, true);
-                break;
-            case kMOVE_DIRECTION.Left :
-            case kMOVE_DIRECTION.LeftUp :
-            case kMOVE_DIRECTION.LeftDown :
-                m_Animator.SetBool(kCHARACTER_IDLE_SIDE, true);
-                break;
-            case kMOVE_DIRECTION.Right :
-            case kMOVE_DIRECTION.RightUp :
-            case kMOVE_DIRECTION.RightDown :
-                this.transform.localScale = new Vector3(-m_BaseCharacterScale.x, m_BaseCharacterScale.y, m_BaseCharacterScale.z);
-                m_Animator.SetBool(kCHARACTER_IDLE_SIDE, true);
-                break;            
-        }
+        string directionName = GetDirectionSpriteName();
+
+        m_SpriteCharacter.sprite = m_SpriteAtlasCharacter.GetSprite(string.Format("{0}{1}_{2}", kSPRITE_NAME_CHARATER, directionName, index));
+        m_SpriteTop.sprite = m_SpriteAtlasCharacter.GetSprite(string.Format("{0}{1}_{2}", kSPRITE_NAME_TOP, directionName, index));
+        m_SpritePants.sprite = m_SpriteAtlasCharacter.GetSprite(string.Format("{0}{1}_{2}", kSPRITE_NAME_PANTS, directionName, index));
+        m_SpriteEyes.sprite = m_SpriteAtlasCharacter.GetSprite(string.Format("{0}{1}_{2}", kSPRITE_NAME_EYES, directionName, index));
+        m_SpriteHair.sprite = m_SpriteAtlasCharacter.GetSprite(string.Format("{0}{1}_{2}", kSPRITE_NAME_HAIR, directionName, index));
     }
 
     private void Move(kMOVE_DIRECTION direciton)    
     {
-        ResetWalkParameter();
-        ResetIdleParameter();
+        ResetAnimatorParameter();
         
-        // 정지한 경우.
-        if (direciton == kMOVE_DIRECTION.None)
-        {
-            m_CurrentState = kCHARACTER_STATE.Idle;
-            return ;
-        }
-        else
-        {
-            m_CurrentState = kCHARACTER_STATE.Walk;
-        }
-
         m_CurrentDirection = direciton;
-        this.transform.localScale = m_BaseCharacterScale;
 
-        switch (direciton)
+        m_CurrentState = kCHARACTER_STATE.Walk;
+        m_Animator.SetBool(kPARAMETER_NAME_WALK, true);
+        
+        switch (m_CurrentDirection)
+        {
+            case kMOVE_DIRECTION.Right :
+            case kMOVE_DIRECTION.RightDown :
+            case kMOVE_DIRECTION.RightUp :
+                this.transform.localScale = new Vector3(-m_BaseCharacterScale.x, m_BaseCharacterScale.y, m_BaseCharacterScale.z);
+                break;
+            default :
+                this.transform.localScale = m_BaseCharacterScale;
+                break;
+        }
+
+        // 이동 로직 추가.
+    }
+
+    private void StopMove()
+    {
+        ResetAnimatorParameter();
+
+        m_CurrentState = kCHARACTER_STATE.Idle;
+
+        // todo :: Idle 애니메이션이 추가되면 수정.
+        //m_Animator.SetBool(kPARAMETER_NAME_IDLE, true);
+    }
+
+    private void ResetAnimatorParameter()
+    {
+        string[] arrayParameterName = {kPARAMETER_NAME_WALK};
+
+        for (int i = 0; i < arrayParameterName.Length; i++)
+        {
+            string parameter = arrayParameterName[i];
+            m_Animator.SetBool(parameter, false);
+        }          
+    }
+
+    private string GetDirectionSpriteName()
+    {
+        switch (m_CurrentDirection)
         {
             case kMOVE_DIRECTION.Up :
-                break;
+                return kSPRITE_NAME_DIRECTION_BACK;
             case kMOVE_DIRECTION.Down :
-                m_Animator.SetBool(kCHARACTER_WALK_FRONT, true);
-                break;
-            case kMOVE_DIRECTION.Left :
-            case kMOVE_DIRECTION.LeftUp :
-            case kMOVE_DIRECTION.LeftDown :
-                m_Animator.SetBool(kCHARACTER_WALK_SIDE, true);
-                break;
-            case kMOVE_DIRECTION.Right :
-            case kMOVE_DIRECTION.RightUp :
-            case kMOVE_DIRECTION.RightDown :
-                this.transform.localScale = new Vector3(-m_BaseCharacterScale.x, m_BaseCharacterScale.y, m_BaseCharacterScale.z);
-                m_Animator.SetBool(kCHARACTER_WALK_SIDE, true);
-                break;
-        }
-    }
-
-    private void ResetWalkParameter()
-    {
-        string[] arrayParameterName = {kCHARACTER_WALK_FRONT, kCHARACTER_WALK_SIDE};
-
-        for (int i = 0; i < arrayParameterName.Length; i++)
-        {
-            string parameter = arrayParameterName[i];
-            m_Animator.SetBool(parameter, false);
-        }      
-    }
-
-    private void ResetIdleParameter()
-    {
-        string[] arrayParameterName = {kCHARACTER_IDLE_BACK, kCHARACTER_IDLE_FRONT, kCHARACTER_IDLE_SIDE};
-
-        for (int i = 0; i < arrayParameterName.Length; i++)
-        {
-            string parameter = arrayParameterName[i];
-            m_Animator.SetBool(parameter, false);
+                return string.Empty;
+            default :
+                return kSPRITE_NAME_DIRECTION_SIDE;
         }
     }
 }
